8ab9f5ee64fea202ded7268b8b02fecc1fa95d78
feritarou <schwarz.f@web.de>
2024-07-17 11:48:56 +0200

    fix: CI

--- ts/math/partial/dimension.ts
+++ ts/math/partial/dimension.ts
@@ -1,4 +1,4 @@
-import type { JSONSchema } from "json-schema-to-ts"
+import type { JSONSchema } from "json-schema-to-ts";
 
 export default {
   "$id": "https://mastory.io/schemas/math/partial/dimension.json",
