077b63d8c0e0d0f137d5f34c7abb396f40895416
feritarou <schwarz.f@web.de>
2024-11-17 16:22:10 +0100

    feat: unified label schema

---
+++ .github/workflows/typedoc-to-json-description.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+sed -n '
+  # If not start of comment, print line as-is and continue
+  /\/\*\*/!{
+    p
+    b
+  }
+  # Found start of comment - begin collecting
+  {
+    # Clear hold space and put first line there
+    h
+    # Read next line
+    n
+    # While we have not reached end of comment
+    :collect
+    /\*\//!{
+      # Append line to hold space
+      H
+      # Get next line and continue collecting
+      n
+      b collect
+    }
+    # We found the end - append it too
+    H
+    # Get everything from hold space
+    g
+    # Now transform the entire block:
+    # 1. Remove /** and */
+    s/\/\*\*[[:space:]]*\n//
+    s/\n[[:space:]]*\*\///
+    # 2. Remove leading asterisks and spaces from middle lines
+    s/\n[[:space:]]*\*[[:space:]]*/ /g
+    # 3. Trim spaces
+    s/^[[:space:]]*\*\?[[:space:]]*\|[[:space:]]*$//
+    # 4. Escape any existing double quotes
+    s/"/\\"/g
+    # 5. Format as $comment property
+    s/.*/"description": "&",/
+    # Print result
+    p
+    # Continue with next line
+    b
+  }
+'
--- ts/fragment/partial/CsCircle.ts
+++ ts/fragment/partial/CsCircle.ts
@@ -1,8 +1,48 @@
 import type { JSONSchema } from "json-schema-to-ts";
+import labelConfig from './label-config'
+
 export default {
   "properties": {
+    "label": {
+      /**
+       * The label shown next to this fragment.
+       * 
+       * You can use shortcuts to configure one label with default positioning,
+       * or use object notation to define separate label behaviors for each
+       * available position.
+       * 
+       * - If `true`, always display a label with default text, visibility and style.
+       * - If `false`, always hide this label.
+       * - If set to a string, this defines the label text.
+       * - If set to an object, you can define text, visibility and style separately.
+       */
+      "oneOf": [
+        {
+          "$ref": "./label-config.json"
+        } as unknown as typeof labelConfig,
+        {
+          "type": "object",
+          "properties": {
+            "center": {
+              /**
+               * The circle center. 
+               */
+              "$ref": "./label-config.json"
+            } as unknown as typeof labelConfig
+          },
+          "additionalProperties": false
+        }
+      ]
+    },
     "radius": {
-      "type": "number"
+      "oneOf": [
+        {
+          "type": "string"
+        },
+        {
+          "type": "number"
+        }
+      ]
     }
   },
   "required": [
