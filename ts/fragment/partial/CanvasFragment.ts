75602abd4bce2a0c50344245fb943e3cae47ea25
feritarou <schwarz.f@web.de>
2024-11-18 00:57:19 +0100

    feat: add LineSegment + support color property in CanvasFragments

--- ts/fragment/partial/CanvasFragment.ts
+++ ts/fragment/partial/CanvasFragment.ts
@@ -1,5 +1,17 @@
 import type { JSONSchema } from "json-schema-to-ts";
+import color from '../../core/color'
+
 export default {
-  "properties": {},
+  "properties": {
+    "color": {
+      /**
+       * The color of this element.
+       * 
+       * NOTE: Currently, the only format reliably supported is
+       * hexadecimal, for example `#a347bf`.
+       */
+      "$ref": "../core/color.json"
+    } as unknown as typeof color
+  },
   "required": []
 } as const satisfies JSONSchema
