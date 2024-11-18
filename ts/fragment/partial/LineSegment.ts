2a735ff3ca8e924f9c06b25032be73a186e16ed2
feritarou <schwarz.f@web.de>
2024-11-18 01:25:23 +0100

    feat: add startPoint/endPoint properties to LineSegment

--- ts/fragment/partial/LineSegment.ts
+++ ts/fragment/partial/LineSegment.ts
@@ -1,5 +1,33 @@
 import type { JSONSchema } from "json-schema-to-ts";
 export default {
-  "properties": {},
+  "properties": {
+    "startPoint": {
+      /**
+       * One of the two points that delimit the segment.
+       * 
+       * Currently, this must be a string matching the
+       * ID of an existing Point fragment.
+       */
+      "oneOf": [
+        {
+          "type": "string"
+        }
+      ]
+    },
+    "endPoint": {
+      /**
+       * One of the two points that delimit the segment.
+       * 
+       * Currently, this must be a string matching the
+       * ID of an existing Point fragment.
+       */
+      "oneOf": [
+        {
+          "type": "string"
+        }
+      ]
+    }
+
+  },
   "required": []
 } as const satisfies JSONSchema
