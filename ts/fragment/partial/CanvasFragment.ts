89639474178e695d0c37eabc8daa85f2a9847e3e
feritarou <schwarz.f@web.de>
2024-12-11 19:07:06 +0100

    feat: support coordinate maps in Canvas+RulerTool

--- ts/fragment/partial/CanvasFragment.ts
+++ ts/fragment/partial/CanvasFragment.ts
@@ -3,6 +3,23 @@ import color from '../../core/color'
 
 export default {
   "properties": {
+    "activeMap": {
+      /**
+       * The currently selected coordinates map.
+       * 
+       * Must be set to one of the keys in the containing canvas's "maps"
+       * object, or "pixels".
+       * 
+       * If set, all coordinate values got or set for this fragment
+       * will be translated back and forth between
+       * pixel coordinates and the coordinate units
+       * defined by the selected map.
+       * 
+       * If not set, defaults to the value of the
+       * containing canvas's "defaultMapForChildren" property.
+       */
+      "type": "string"
+    },
     "color": {
       /**
        * The color of this element.
--- ts/fragment/partial/RulerTool.ts
+++ ts/fragment/partial/RulerTool.ts
@@ -1,5 +1,23 @@
 import type { JSONSchema } from "json-schema-to-ts";
 export default {
-  "properties": {},
+  "properties": {
+    "activeMap": {
+      /**
+       * The currently selected coordinates map.
+       * 
+       * Must be set to one of the keys in the containing canvas's "maps"
+       * object, or "pixels".
+       * 
+       * If set, coordinate values measured with this tool
+       * will be translated between
+       * pixel coordinates and the coordinate units
+       * defined by the selected map.
+       * 
+       * If not set, defaults to the value of the
+       * containing canvas's "defaultMapForChildren" property.
+       */
+      "type": "string"
+    },
+  },
   "required": []
 } as const satisfies JSONSchema
