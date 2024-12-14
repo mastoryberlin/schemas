52215c6847d839eb912ef3d1d19b7da6fa4d62de
feritarou <schwarz.f@web.de>
2024-12-14 19:56:47 +0100

    feat: add canvasActivityWhileSelected prop to CanvasTool fragment

--- ts/fragment/partial/CanvasTool.ts
+++ ts/fragment/partial/CanvasTool.ts
@@ -1,17 +1,18 @@
 import type { JSONSchema } from "json-schema-to-ts";
 import url from '../../core/url'
+import type identifier from "./identifier";
 
 export default {
   /**
-  * A tool to interact with a coordinate system.
+  * A tool to interact with a canvas.
   * 
-  * Fragments of this class will only work when placed directly under a `CoordinateSystem` fragment.
+  * Fragments of this class will only work when placed directly under a `Canvas` fragment.
   *
   * @example
   * ```jsonc
   * {
   *   "id": "myTool",
-  *   "class": "CsTool",
+  *   "class": "CanvasTool",
   *   "text": "My New Tool",
   *   "icon": "https://mastory.io/my-tool-icon.png"
   * }
@@ -29,7 +30,18 @@ export default {
        * An icon to show on the toolbar button.
        */
       "$ref": "../../core/url.json"
-    } as unknown as typeof url
+    } as unknown as typeof url,
+    "canvasActivityWhileSelected": {
+      /**
+       * If set, the tool will temporarily replace its parent canvas's
+       * `controllingActivity` by the provided fragment ID 
+       * when selected, and reset it to the previous value when deselected.
+       * 
+       * This can be useful to bind more specific support and instructions
+       * to the parent canvas while it is being used with this tool.
+       */
+      "$ref": "./identifier.json"
+    } as unknown as typeof identifier
   },
   "required": [
     "icon"
