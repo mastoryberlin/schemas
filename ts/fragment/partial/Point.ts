76585ec78c5c4abdd4c5861cdb3da791f118c391
feritarou <schwarz.f@web.de>
2024-12-15 05:25:28 +0100

    chore: move label prop to UiFragment

--- ts/fragment/partial/Point.ts
+++ ts/fragment/partial/Point.ts
@@ -1,19 +1,6 @@
 import type { JSONSchema } from "json-schema-to-ts";
-import labelConfig from './label-config'
 
 export default {
-  "properties": {
-    "label": {
-      /**
-       * The label shown next to this fragment.
-       * 
-       * - If `true`, always display a label with default text, visibility and style.
-       * - If `false`, always hide this label.
-       * - If set to a string, this defines the label text.
-       * - If set to an object, you can define text, visibility and style separately.
-       */
-      "$ref": "./label-config.json"
-    } as unknown as typeof labelConfig
-  },
+  "properties": {},
   "required": []
 } as const satisfies JSONSchema
--- ts/fragment/partial/UiFragment.ts
+++ ts/fragment/partial/UiFragment.ts
@@ -1,9 +1,21 @@
 import type { JSONSchema } from "json-schema-to-ts";
+import labelConfig from './label-config'
 import allowClick from '../../core/allow/click'
 import allowSelect from '../../core/allow/select'
 
 export default {
   "properties": {
+    "label": {
+      /**
+       * The label shown next to this fragment.
+       * 
+       * - If `true`, always display a label with default text, visibility and style.
+       * - If `false`, always hide this label.
+       * - If set to a string, this defines the label text.
+       * - If set to an object, you can define text, visibility and style separately.
+       */
+      "$ref": "./label-config.json"
+    } as unknown as typeof labelConfig,
     "allow": {
       /**
        * Sets behavioral policies, such as: can this element be clicked? selected? dragged away?
