a381208d7e3601211f5aba3bc7c49556b6b3e9c1
feritarou <schwarz.f@web.de>
2024-12-20 01:35:38 +0100

    fix: Canvas fragment

--- ts/fragment/partial/Canvas.ts
+++ ts/fragment/partial/Canvas.ts
@@ -191,7 +191,7 @@ export default {
                           },
                           "required": ["x", "y"],
                           "additionalProperties": false
-                        },
+                        }
                       ]
                     }
                   }
